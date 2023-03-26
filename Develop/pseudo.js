35a3998359b03ee6d5134a9f4bf21a64c87fddf7
Chelsey Valerio <chelseyvalerio@Chelseys-MacBook-Pro.local>
2023-03-26 16:32:46 -0600

    first push with models inital development pushed

---
+++ Develop/pseudo.js
@@ -0,0 +1,31 @@
+/**
+ * ecommerce backend app
+ * 
+ * In INSOMNIA:
+ * --- Products folder ----
+ * GET all products
+ * GET single product by id
+ * PUT create new product --- make sure to reference models and pass in association
+ * POST update product by id
+ * DELETE product
+ * 
+ * ---Categories folder---
+ * GET all categories
+ * GET single category bu ID
+ * PUT create new category reference models
+ * POST upate category by id
+ * DELETE category
+ * 
+ * ----Tags folder ---
+ * GET all tags
+ * GET single tag by ID
+ * PUT Create new tag reference models
+ * POST update tag by id
+ * DELETE tag
+ * 
+ * 
+ * deploy to heroku
+ * 
+ * for submission data, start from very beginning __ source schema --> npm i and npm start, then go to insomnia and send off each call
+ * 
+ */
