2bf27dee3afcaeed09c2733aa06bcf46ce100580
chelseyvalerio <cvalerio@csog.net>
2023-04-03 11:15:33 -0600

    tester push

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
-
+// testing git 
 // get all products
 router.get('/', async (req, res) => {
   // find all products
